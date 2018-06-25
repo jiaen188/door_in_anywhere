e6c5904e8cad7a197b48a88330ba2d21805f39b2
jiaen188 <244189981@qq.com>
2018-06-25 20:22:58 +0800

    代码优化

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
+const path = require('path');
+const Handlebars = require('handlebars');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+const config = require('../config/defaultConfig');
+const mime = require('./mime');
+
+const tplPath = path.join(__dirname, '../template/dir.tpl');
+// 获取模板的绝对路径
+const source = fs.readFileSync(tplPath);
+const template = Handlebars.compile(source.toString());
 
 module.exports = async function (req, res, filePath) {
     try {
         const stats = await stat(filePath);
         if (stats.isFile()) {
+            const contentType = mime(filePath);
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
+            res.setHeader('Content-Type', contentType);
             // 速度太慢,所以用流的方式
             /* fs.readFile(filePath, (err, data) => {
                 res.end(data);
@@ -17,8 +27,14 @@ module.exports = async function (req, res, filePath) {
         } else if (stats.isDirectory()) {
             const files = await readdir(filePath);
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(files.join(','));
+            res.setHeader('Content-Type', 'text/html');
+            const dir = path.relative(config.root, filePath);
+            const data = {
+                title: path.basename(filePath),
+                dir: dir ? `/${dir}` : '',
+                files: files.map(file => ({file, icon: mime(file)}))
+            };
+            res.end(template(data));
         }
     } catch (error) {
         console.error(error);
