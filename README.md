881b5bb17cacd6ec02f30f794e736301e0908fed
jiaen188 <244189981@qq.com>
2018-06-27 14:10:17 +0800

    改写成cli格式，可以发布到npm

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# anydoor
+# door_in_anywhere
 NodeJS Static Web server
 
-
 ##使用方法
+
+```
+door_in_anywhere # 把当前文件夹作为静态资源服务器根目录
+
+door_in_anywhere -p 8080 # 设置端口号为 8080
+
+door_in_anywhere -h localhost # 设置 host 为 localhost
+
+door_in_anywhere -d /user # 设置根目录为 /user
+```
--- src/helper/route.js
+++ src/helper/route.js
@@ -4,7 +4,7 @@ const Handlebars = require('handlebars');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
-const config = require('../config/defaultConfig');
+// const config = require('../config/defaultConfig');
 const mime = require('./mime');
 const compress = require('./compress');
 const range = require('./range');
@@ -15,7 +15,7 @@ const tplPath = path.join(__dirname, '../template/dir.tpl');
 const source = fs.readFileSync(tplPath);
 const template = Handlebars.compile(source.toString());
 
-module.exports = async function (req, res, filePath) {
+module.exports = async function (req, res, filePath, config) {
     try {
         const stats = await stat(filePath);
         if (stats.isFile()) {
